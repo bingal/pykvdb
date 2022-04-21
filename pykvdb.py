50860d764b23e4855100baef51de20f9ac88260f
bingal <bingalsky@gmail.com>
2022-04-21 14:10:23 +0800

    update version 0.1.1

--- pykvdb.py
+++ pykvdb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # encoding: utf-8
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Bingal'
 
 import lmdb
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/bingal/pykvdb'
 EMAIL = 'bingalsky@gmail.com'
 AUTHOR = 'Bingal'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
